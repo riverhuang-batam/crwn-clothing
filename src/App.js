bdbab7955c14640eca3fb2c0bc3a43c7116fee8d
riverhuang-batam <riverhuang011@gmail.com>
2019-12-30 14:24:32 +0700

    update our menu-items to navigate withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,15 +1,55 @@
 import React, {Component} from 'react';
+import {Route, Switch, Link} from 'react-router-dom'
 import {HomePages} from './pages/homepage/homepage.component'
 import './App.css';
 
-export default class App extends Component{
-  render(){
+const HomePage = (props) => {
+    console.log(props)
     return (
-      <div>
-        <HomePages/>
-      </div>
+        <div>
+            <Link to='/topicsList'>
+                <h1>Home Page</h1>
+            </Link>
+        </div>
     )
-  }
 }
+const TopicsList = (props) => {
+    console.log(props)
+    return (
+        <div>
+            <h1>TopicList Pages</h1>
+        </div>
+    )
+}
+const hats = (props) => {
+  console.log(props)
+  return (
+      <div>
+          <h1>hats</h1>
+      </div>
+  )
+}
+const TopicDetail = (props) => {
 
+    return (
+        <div>
+            <h1>Topic Details PAGES: {props.match.params.TopicDetail}</h1>
+        </div>
+    )
+}
+export default class App extends Component {
 
+    render() {
+        return (
+            <div>
+                <Switch>
+                    <Route exact path="/" component={HomePages}/>
+                    <Route exact path="/HomePage" component={HomePage}/>
+                    <Route exact path="/TopicsList" component={TopicsList}/>
+                    <Route exact path="/hats" component={hats}/>
+                    <Route path="/TopicsList/:TopicDetail" component={TopicDetail}/>
+                </Switch>
+            </div>
+        )
+    }
+}
